a13919c950d56f027f68d44d5d91cc543bc98779
domien.nowicki <domien.nowicki@a23ea291-9b30-0410-92ec-93a989225022>
2007-05-16 19:46:24 +0000

    First commit - some parser stuff

---
+++ trunk/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Main.java
+ *
+ * Created on 13 mei 2007, 21:56
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+//package regexp;
+/**
+ *
+ * @author neimod
+ */
+public class Main
+{
+
+	/** Creates a new instance of Main */
+	public Main()
+	{
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		Parser p = new Parser("a*.b|c");
+		
+		
+	}
+
+}
